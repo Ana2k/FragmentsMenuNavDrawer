bafd6d617856f055935d845294ffaeb28a817d68
Anushka Chakraborty <anushkachakra@gmail.com>
2021-07-18 18:20:35 +0530

    replaceFragment

--- app/src/main/java/com/example/fragmentsyt/MainActivity.kt
+++ app/src/main/java/com/example/fragmentsyt/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.fragmentsyt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.fragmentsyt.databinding.ActivityMain2Binding
 
 class MainActivity : AppCompatActivity() {
@@ -14,5 +15,14 @@ class MainActivity : AppCompatActivity() {
         _binding = ActivityMain2Binding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        val fragment = FirstImageFragment.newInstance()
+        replaceFragment(fragment)
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainerView,fragment)
+        fragmentTransaction.commit()
     }
 }
